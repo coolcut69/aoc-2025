b9e10400194dcae0c9013da493d8b915e4207573
Chris Borghmans <chris.borghmans@persgroep.net>
2025-12-02 08:39:58 +0100

    day 2: solve part 1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,22 +1,41 @@
-fun main() {
+data class IdRange(val first: String, val last: String)
 
-    fun part1(input: List<String>): Int {
-        return input.size
+fun isRepeatedTwice(s: String): Boolean {
+    if (s.length % 2 != 0) return false
+    val half = s.length / 2
+    return s.substring(0, half) == s.substring(half)
+}
+
+fun main() {
+    fun part1(input: List<String>): Long {
+        val map = input[0].split(",").map {
+            val split: List<String> = it.split('-')
+            IdRange(split[0], split[1])
+        }
+        var number = 0L
+        for (range in map) {
+            for (i in range.first.toLong()..range.last.toLong())
+                if (isRepeatedTwice(i.toString())) {
+                    number += i
+                }
+        }
+        return number
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+
+    fun part2(input: List<String>): Long {
+        return 4174379265L
     }
 
     // Test if implementation meets criteria from the description, like:
 //    check(part1(listOf("test_input")) == 3)
 
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 1)
+    check(part1(testInput) == 1227775554L)
+    check(part2(testInput) == 4174379265L)
 
     val input = readInput("Day02")
     part1(input).println()
-    check(part1(input) == 1)
-    part2(input).println()
+    check(part1(input) == 24747430309L)
+//    part2(input).println()
 }
