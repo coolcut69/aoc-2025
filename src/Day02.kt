ce09e31a9f257762c59ef877a88e0be94a257097
Chris Borghmans <chris.borghmans@persgroep.net>
2025-12-02 13:10:30 +0100

    day 2: solve part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -3,7 +3,17 @@ data class IdRange(val first: String, val last: String)
 fun isRepeatedTwice(s: String): Boolean {
     if (s.length % 2 != 0) return false
     val half = s.length / 2
-    return s.substring(0, half) == s.substring(half)
+    return s.take(half) == s.substring(half)
+}
+
+fun hasRepeatedSequence(s: String): Boolean {
+    for (len in 1..s.length / 2) {
+        if (s.length % len == 0) {
+            val part = s.take(len)
+            if (part.repeat(s.length / len) == s) return true
+        }
+    }
+    return false
 }
 
 fun main() {
@@ -22,9 +32,19 @@ fun main() {
         return number
     }
 
-
     fun part2(input: List<String>): Long {
-        return 4174379265L
+        val map = input[0].split(",").map {
+            val split: List<String> = it.split('-')
+            IdRange(split[0], split[1])
+        }
+        var number = 0L
+        for (range in map) {
+            for (i in range.first.toLong()..range.last.toLong())
+                if (hasRepeatedSequence(i.toString())) {
+                    number += i
+                }
+        }
+        return number
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -37,5 +57,6 @@ fun main() {
     val input = readInput("Day02")
     part1(input).println()
     check(part1(input) == 24747430309L)
-//    part2(input).println()
+    part2(input).println()
+    check(part2(input) == 30962646823L)
 }
