d71782dd7ff61040331e8af14fc3174a17549df9
Chris Borghmans <chris.borghmans@persgroep.net>
2025-12-05 08:02:00 +0100

    day 5: solve part 1

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,6 +1,22 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val ranges = input.filter { line -> line.contains('-') }.toList()
+        val ingredients = input.subList(ranges.size + 1, input.size).map { it.toLong() }
+
+        var inRange = 0
+
+        for (ingredient in ingredients) {
+            for (range in ranges){
+                val beginRange = range.split('-')[0].toLong()
+                val endRange = range.split('-')[1].toLong()
+
+                if (ingredient in beginRange..endRange) {
+                    inRange++
+                    break;
+                }
+            }
+        }
+        return inRange
     }
 
     fun part2(input: List<String>): Int {
@@ -8,12 +24,12 @@ fun main() {
     }
 
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 3)
     check(part2(testInput) == 0)
 
     val input = readInput("Day05")
     part1(input).println()
-    check(part1(input) == 0)
-    part2(input).println()
-    check(part2(input) == 0)
+    check(part1(input) == 525)
+//    part2(input).println()
+//    check(part2(input) == 0)
 }
